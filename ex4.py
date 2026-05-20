d51e68dd8aa59e33c26f17e36746e53ceff6ce8a
Kelvin Gomes D'Beraldini <kelvingomes888@gmail.com>
2026-05-20 19:51:27 -0300

    Update ex4.py

--- ex4.py
+++ ex4.py
@@ -3,10 +3,13 @@ primos = []
 
 for num in range(2, limite + 1):
     e_primo = True
+    
     for i in range(2, int(num ** 0.5) + 1):
+        
         if num % i == 0:
             e_primo = False
             break
+            
     if e_primo:
         primos.append(num)
 
