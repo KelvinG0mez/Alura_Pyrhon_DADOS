62799757b56d7ab3a54a0066c874fe2dab0a1574
Kelvin Gomes D'Beraldini <kelvingomes888@gmail.com>
2026-05-20 19:45:05 -0300

    Create ex8.py

---
+++ ex8.py
@@ -0,0 +1,11 @@
+bacterias = [1.2, 2.1, 3.3, 5.0, 7.8, 11.3, 16.6, 25.1, 37.8, 56.9]
+crescimento = []
+
+for i in range(1, len(bacterias)):
+
+    atual = bacterias[i]
+    passada = bacterias[i - 1]
+    pct = 100 * (atual - passada) / passada
+    crescimento.append(round(pct, 2))
+
+print(crescimento)
