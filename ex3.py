540ebac1fa9603bf30d6c7a57887fee439c3f62f
Kelvin Gomes D'Beraldini <kelvingomes888@gmail.com>
2026-05-20 19:43:15 -0300

    Create ex3.py

---
+++ ex3.py
@@ -0,0 +1,9 @@
+lista_numeros = []
+
+for i in range(5):
+    numero = int(input(f"Digite o {i+1}º número inteiro: "))
+    
+    lista_numeros.append(numero)
+
+print("\nA lista gerada foi:")
+print(lista_numeros)
